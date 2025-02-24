23fceec6378a28285c6ca77b403c09f4082e9d18
Miraska <102532370+Miraska@users.noreply.github.com>
2025-02-24 18:12:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 const PORT = 3000;
 let fileName;
 
