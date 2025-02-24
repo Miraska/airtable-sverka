e2fbcf21a32c921d820ec9101c4e855f0864db11
Miraska <102532370+Miraska@users.noreply.github.com>
2025-02-24 18:48:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 const PORT = 3000;
 let fileName;
